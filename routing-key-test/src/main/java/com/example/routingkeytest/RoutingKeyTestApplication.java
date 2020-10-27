ba890ac889d2c334ed1049d0b32d210b18d1346a
Michał Zalewski <mzalewski@ravenpack.com>
2020-10-27 13:00:58 +0100

    Initial commit

---
+++ routing-key-test/src/main/java/com/example/routingkeytest/RoutingKeyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.routingkeytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingKeyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutingKeyTestApplication.class, args);
+    }
+
+}
---
+++ routing-key-test/src/test/java/com/example/routingkeytest/RoutingKeyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routingkeytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingKeyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
